0ecb4c6539cbc489a2eaad83d62afa841f429632
Ethiy <oussama.ennafii@ign.fr>
2018-03-19 11:53:47 +0100

    crop + filter bank refactored

--- scatwave/filters_bank.py
+++ scatwave/filters_bank.py
@@ -9,61 +9,114 @@ import torch
 import numpy as np
 import scipy.fftpack as fft
 
+import operator
+import functools
 
 
+def band_filter(fourier_signal, res):
+    cropped_signal = crop_freq(fourier_signal, res)
+    return torch.FloatTensor(
+        np.stack(
+            (
+                np.real(cropped_signal),
+                np.imag(cropped_signal)
+            ),
+            axis=2
+        )
+    )
 
-def filters_bank(M, N, J, L=8):
-    filters = {}
-    filters['psi'] = []
 
-
-    offset_unpad = 0
-    for j in range(J):
-        for theta in range(L):
-            psi = {}
-            psi['j'] = j
-            psi['theta'] = theta
-            psi_signal = morlet_2d(M, N, 0.8 * 2**j, (int(L-L/2-1)-theta) * np.pi / L, 3.0 / 4.0 * np.pi /2**j,offset=offset_unpad) # The 5 is here just to match the LUA implementation :)
-            psi_signal_fourier = fft.fft2(psi_signal)
-            for res in range(j + 1):
-                psi_signal_fourier_res = crop_freq(psi_signal_fourier, res)
-                psi[res]=torch.FloatTensor(np.stack((np.real(psi_signal_fourier_res), np.imag(psi_signal_fourier_res)), axis=2))
-                # Normalization to avoid doing it with the FFT!
-                psi[res].div_(M*N// 2**(2*j))
-            filters['psi'].append(psi)
-
-    filters['phi'] = {}
-    phi_signal = gabor_2d(M, N, 0.8 * 2**(J-1), 0, 0, offset=offset_unpad)
-    phi_signal_fourier = fft.fft2(phi_signal)
-    filters['phi']['j'] = J
-    for res in range(J):
-        phi_signal_fourier_res = crop_freq(phi_signal_fourier, res)
-        filters['phi'][res]=torch.FloatTensor(np.stack((np.real(phi_signal_fourier_res), np.imag(phi_signal_fourier_res)), axis=2))
-        filters['phi'][res].div_(M*N // 2 ** (2 * J))
-
-    return filters
+def filters_bank(M, N, J, L=8, offset=0):
+    return {
+        'psi': [
+            dict(
+                [
+                    ('j', j),
+                    ('theta', theta)
+                ] + [
+                    (
+                        res,
+                        band_filter(
+                            fft.fft2(
+                                morlet_2d(
+                                    M,
+                                    N,
+                                    4 * pow(2, j) / 5,
+                                    (int(L-L/2-1)-theta) * np.pi / L,
+                                    (3 * np.pi) / (4 * pow(2, j)),
+                                    offset=offset
+                                )
+                            ),
+                            res
+                        )
+                        /
+                        (M * N // pow(2, 2*j))
+                    )
+                    for res in range(1 + j)
+                ]
+            )
+            for j in range(J) for theta in range(L)
+        ],
+        'phi': dict(
+            [('j', J)] + [
+                (
+                    res,
+                    band_filter(
+                        fft.fft2(
+                            gabor_2d(
+                                M,
+                                N,
+                                4 * pow(2, J - 1) / 5,
+                                0,
+                                0,
+                                offset=offset
+                            )
+                        ),
+                        res
+                    ) / (M * N // pow(2, 2 * J))
+                )
+                for res in range(J)
+            ]
+        )
+    }
 
 
 def crop_freq(x, res):
-    M = x.shape[0]
-    N = x.shape[1]
-
-    crop = np.zeros((M // 2 ** res, N // 2 ** res), np.complex64)
+    M, N = x.shape[:2]
 
     mask = np.ones(x.shape, np.float32)
-    len_x = int(M * (1 - 2 ** (-res)))
-    start_x = int(M * 2 ** (-res - 1))
-    len_y = int(N * (1 - 2 ** (-res)))
-    start_y = int(N * 2 ** (-res - 1))
-    mask[start_x:start_x + len_x,:] = 0
-    mask[:, start_y:start_y + len_y] = 0
-    x = np.multiply(x,mask)
+    len_x = int(M * (1 - pow(2, -res)))
+    start_x = int(M * pow(2, -res - 1))
+    len_y = int(N * (1 - pow(2, -res)))
+    start_y = int(N * pow(2, -res - 1))
+    mask[
+        start_x: start_x + len_x,
+        :
+    ] = 0
+    mask[
+        :,
+        start_y: start_y + len_y
+    ] = 0
+    x = np.multiply(x, mask)
 
-    for k in range(int(M / 2 ** res)):
-        for l in range(int(N / 2 ** res)):
-            for i in range(int(2 ** res)):
-                for j in range(int(2 ** res)):
-                    crop[k, l] += x[k + i * int(M / 2 ** res), l + j * int(N / 2 ** res)]
+    crop = np.zeros(
+        (M // pow(2, res), N // pow(2, res)),
+        np.complex64
+    )
+    for k in range(int(M / pow(2, res))):
+        for l in range(int(N / pow(2, res))):
+            crop[k, l] = functools.reduce(
+                operator.add,
+                [
+                    x[
+                        k + i * int(M / pow(2, res)),
+                        l + j * int(N / pow(2, res))
+                    ]
+                    for i in range(pow(2, res))
+                    for j in range(pow(2, res))
+                ],
+                crop[k, l]
+            )
 
     return crop
 
